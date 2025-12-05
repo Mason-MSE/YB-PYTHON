711489dd1f88ce6e1d646fadacbbe0cf22283c48
Mason <wwwwlqc@gmail.com>
2025-12-06 09:27:20 +1300

    File Operation

--- FileAccess.py
+++ FileAccess.py
@@ -1,3 +1,4 @@
+import os
 class FileAccess:
     def __init__(self, filepath, mode='r'):
         self.filepath = filepath
@@ -21,17 +22,26 @@ class FileAccess:
             #             number+=1
             #             pos=contents.find("*",pos+1)
             #             # print(f'{number} line -----------:{contents}')
-
-
-               print(f'Total lines containing [*]: {number}')
+            print(f'Total lines containing [*]: {number}')
         except FileNotFoundError:
             print(f"Error: The file {self.filepath} was not found.")
         except IOError:
             print(f"Error: Could not read the file {self.filepath}.")
+    def append_to_file(self, text):
+        try:
+            with open(self.filepath, 'a') as file:
+                file.write(text + '\n')
+            print(f"Successfully appended to the file {self.filepath}.")
+        except IOError:
+            print(f"Error: Could not write to the file {self.filepath}.")   
 if __name__ == "__main__":
     filepath = './data/demo_file.txt'
     file_access = FileAccess(filepath, 'r')
     file_access.print_file_contents()
 
-    test="2123123"
-    print(test.find("*"))
+    file_access = FileAccess(filepath, 'a')
+    file_access.append_to_file("End of File")
+
+
+
+    
